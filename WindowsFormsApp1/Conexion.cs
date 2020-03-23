ed4f0dfaaf5e00a17fbd9bcf7fc55b6c1e82ab87
Fonseca1998 <62191695+Fonseca1998@users.noreply.github.com>
2020-03-23 10:23:49 -0600

    guardar

---
+++ WindowsFormsApp1/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    class Conexion
+    {
+        public static SqlConnection conexion = new SqlConnection("workstation id=Fonseca1998.mssql.somee.com;packet size=4096;user id=fonseca1998_SQLLogin_1;pwd=t5fvbjqhdv;data source=fonsecq1998.mssql.somee.com;persist security info=False;initial catalog=fonseca1998");
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConexSql();
+        }
+        public static SqlCommand ComandoSq;
+
+        public void ConexSql()
+        {
+            ComandoSq = new SqlCommand(@"Select * From Clientes", Conexion.conexion);
+            Conexion.conexion.Open();
+            DataTable tabla = new DataTable();
+            SqlDataAdapter usar = new SqlDataAdapter(ComandoSq);
+            usar.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+            Conexion.conexion.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                try
+                {
+                    DialogResult respuesta = MessageBox.Show("¿Desea eliminar el dato?","Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (respuesta == DialogResult.Yes)
+                    {
+                        Conexion.conexion.Open();
+                        ComandoSq.ExecuteNonQuery();
+                        ComandoSq.CommandText = "Delete From Productos Where id_cliente = " + dataGridView1.CurrentRow.Cells[0].Value.ToString() + "; ";
+                        ComandoSq.Connection = Conexion.conexion;
+                        Conexion.conexion.Close();
+                        MessageBox.Show("El Cliente fue Eliminado Exitosamente");
+                        ConexSql();
+                    }
+                    else if (respuesta == DialogResult.No) ConexSql();
+                }
+                catch (Exception oo)
+                {
+                    Conexion.conexion.Close();
+                    MessageBox.Show(oo.ToString());
+                }
+            }
+            else MessageBox.Show("Favor de seleeccionar un dato de la tabla");
+        }
+    }
+}
